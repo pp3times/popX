64fa35b9c7ad160913dbce5db7671fa636d8f73e
Woramat Ngamkham <woramat.ng@gmail.com>
2021-11-13 03:16:50 +0700

    :lipstick: Update style

--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POPCAT</title>
     <link rel="shortcut icon" type="image/jpg" href="favicon.ico" />
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css" integrity="sha512-Cv93isQdFwaKBV+Z4X8kaVBYWHST58Xb/jVOcV9aRsGSArZsgAnFIhMpDoMDcFNoUtday1hdjn0nGp3+KZyyFw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
@@ -50,10 +50,10 @@
         <!-- <img src="maincat1.png" alt="Invalid" id="popcat1" height="500x"> -->
         <img src="./assets/image/maincat1.png" alt="Invalid" id="popcat1" height="600px">
         <div class="badge bg-primary text-wrap topright">
-            เหนือ: <a id="demo_1">1</a><br><br>
-            กลาง: <a id="demo_2">1</a><br><br>
-            อีสาน: <a id="demo_3">1</a><br><br>
-            ใต้: <a id="demo_4">1</a><br><br>
+            เหนือ: <a id="demo_1">0</a><br><br>
+            กลาง: <a id="demo_2">0</a><br><br>
+            อีสาน: <a id="demo_3">0</a><br><br>
+            ใต้: <a id="demo_4">0</a><br><br>
         </div>
         <select name="majar" id="majar">
             <option value="0">[0] เหนือ</option> <!-- value 0 -->
