bcca54fc74040c6a5629d466057e58d51533756a
Woramat Ngamkham <woramat.ng@gmail.com>
2021-11-13 00:05:59 +0700

    :sparkles: Add k6

---
+++ k6/run-production.js
@@ -0,0 +1,38 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+const PRODUCTION_URL = "http://inspired-it.th1.proen.cloud";
+
+const GET_VALUE_0 = `${PRODUCTION_URL}/src/getValue/getValue_Klang.php`;
+const GET_VALUE_1 = `${PRODUCTION_URL}/src/getValue/getValue_Esan.php`;
+const GET_VALUE_2 = `${PRODUCTION_URL}/src/getValue/getValue_Nuea.php`;
+const GET_VALUE_3 = `${PRODUCTION_URL}/src/getValue/getValue_Tai.php`;
+const UPDATE_ID_0 = `${PRODUCTION_URL}/src/updateValue/UPDATE_ID_0.php`;
+const UPDATE_ID_1 = `${PRODUCTION_URL}/src/updateValue/UPDATE_ID_1.php`;
+const UPDATE_ID_2 = `${PRODUCTION_URL}/src/updateValue/UPDATE_ID_2.php`;
+const UPDATE_ID_3 = `${PRODUCTION_URL}/src/updateValue/UPDATE_ID_3.php`;
+
+export const options = {
+  stages: [
+    { duration: "30s", target: 10 },
+    { duration: "2m30s", target: 100 },
+    { duration: "5m00s", target: 500 },
+  ],
+};
+
+const testLoadWeb = () => {
+  const response = http.batch([
+    ["GET", GET_VALUE_0, null, { tags: { ctype: "html" } }],
+    ["GET", GET_VALUE_1, null, { tags: { ctype: "html" } }],
+    ["GET", GET_VALUE_2, null, { tags: { ctype: "html" } }],
+    ["GET", GET_VALUE_3, null, { tags: { ctype: "html" } }],
+    ["GET", UPDATE_ID_0, null, { tags: { ctype: "html" } }],
+    ["GET", UPDATE_ID_1, null, { tags: { ctype: "html" } }],
+    ["GET", UPDATE_ID_2, null, { tags: { ctype: "html" } }],
+    ["GET", UPDATE_ID_3, null, { tags: { ctype: "html" } }],
+  ]);
+
+  sleep(1);
+};
+
+export default testLoadWeb;
