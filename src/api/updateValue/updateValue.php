f55555a83c8b34dc0f5e0c0da250e023dc54136b
Woramat Ngamkham <woramat.ng@gmail.com>
2021-11-13 08:25:58 +0700

    :zap: Validate update request

--- src/api/updateValue/updateValue.php
+++ src/api/updateValue/updateValue.php
@@ -20,12 +20,13 @@ $regions = array("nuea", "klang", "esan", "tai");
 if (isset($data->region) && isset($data->add)) {
     $region = $data->region;
     $add = $data->add;
-    if (in_array($region, $regions) && $add >= 0) {
-        $sql = "UPDATE `popcat` SET `$region` = `$region` + $add WHERE `popcat` = 1";
-        $conn->query($sql);
+    if (in_array($region, $regions) && $add >= 0 && $add <= 100) {
+        $sql = $conn->prepare("UPDATE `popcat` SET `?` = `?` + ? WHERE `popcat` = 1");
+        $sql->bind_param("ssi", $region, $region, $add);
+        $sql->execute();
         $conn->close();
     } else {
-        echo 0;
+        echo -1;
     }
 } else {
     echo 0;
